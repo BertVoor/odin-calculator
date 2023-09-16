0838a6f2188d3c088860eb086237dc8f2aee4508
Bert <voorjansbert@gmail.com>
2023-09-16 14:29:05 +0200

    Fix font, align screen text.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("script loaded");
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+	number.addEventListener("click", (number) => {
+		firstNumber += number.target.textContent;
+		console.log(firstNumber);
+	});
+});
