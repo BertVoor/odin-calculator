db0e8ec3e4c6aa19fc9213cf181fbfb25f65c898
Bert <voorjansbert@gmail.com>
2023-09-16 14:32:24 +0200

    Revert "Fix font, align screen text."
    This reverts commit 0838a6f2188d3c088860eb086237dc8f2aee4508.
    
    wrong file in commit

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 console.log("script loaded");
-let firstNumber = "";
-let operator = "";
-let secondNumber = "";
-
-const numbers = document.querySelectorAll(".number");
-
-numbers.forEach((number) => {
-	number.addEventListener("click", (number) => {
-		firstNumber += number.target.textContent;
-		console.log(firstNumber);
-	});
-});
