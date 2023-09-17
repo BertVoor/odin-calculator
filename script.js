2d757e70e11aa434d26f8ad48f5d6ae035e2bbd0
Bert <voorjansbert@gmail.com>
2023-09-17 04:16:02 +0200

    Add functionality to AC button.

--- script.js
+++ script.js
@@ -9,34 +9,39 @@ const screenTop = document.querySelector("#screenTop");
 const screenBottom = document.querySelector("#screenBottom");
 const screenContainer = document.querySelector("#screenContainer");
 const operators = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#clear");
 
 setFontSizes();
 window.addEventListener("resize", setFontSizes);
 
 buttons.forEach((button) => {
 	button.addEventListener("click", (button) => {
-		const value = button.target.textContent;
-		displayValue += value;
-		updateScreen(displayValue, "bottom");
+		if (button.target.id == "clear") {
+			clearCalculator();
+		} else {
+			const value = button.target.textContent;
+			displayValue += value;
+			updateScreen(displayValue, "bottom");
 
-		if (button.target.classList.contains("operator")) {
-			operator = value;
-		}
-		if (button.target.className == "number") {
-			if (!operator) {
-				firstNumber += value;
-			} else {
-				secondNumber += value;
+			if (button.target.classList.contains("operator")) {
+				operator = value;
 			}
-		}
-		if (button.target.id == "equals") {
-			updateScreen(displayValue, "top");
-			let result = operate(operator, firstNumber, secondNumber);
-			if (result.toString().length > 10) {
-				const i = result.toString().indexOf(".");
-				result = result.toFixed(9 - i);
+			if (button.target.className == "number") {
+				if (!operator) {
+					firstNumber += value;
+				} else {
+					secondNumber += value;
+				}
 			}
-			updateScreen(result, "bottom");
+			if (button.target.id == "equals") {
+				updateScreen(displayValue, "top");
+				let result = operate(operator, firstNumber, secondNumber);
+				if (result.toString().length > 10) {
+					const i = result.toString().indexOf(".");
+					result = result.toFixed(9 - i);
+				}
+				updateScreen(result, "bottom");
+			}
 		}
 	});
 });
@@ -69,6 +74,7 @@ function operate(operator, firstNumber, secondNumber) {
 			total = divide(firstNumber, secondNumber);
 			break;
 	}
+	console.log(total);
 	return total;
 }
 function updateScreen(displayValue, screen) {
@@ -81,4 +87,13 @@ function setFontSizes() {
 	let width = screenContainer.offsetWidth;
 	screenTop.style.fontSize = `${width / 9.2}px`;
 	screenBottom.style.fontSize = `${width / 4.6}px`;
+}
+
+function clearCalculator() {
+	firstNumber = "";
+	secondNumber = "";
+	displayValue = "";
+	operator = null;
+	updateScreen("", "top");
+	updateScreen("0", "bottom");
 }
